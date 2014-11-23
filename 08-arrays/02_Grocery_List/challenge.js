02fc240ae3b4b11fc694b5753eca25be2034c531
anomalophobe <01b6e6@gmail.com>
2014-11-23 05:17:27 +0000

    Updated Grocery List exercise

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,17 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function (itemToAdd, myList){
+  var listLength = myList.length;
+  var i = 0;
+  for (i; i < listLength; i++){
+    if (itemToAdd === myList[i]){
+      return myList;
+    } else {
+      myList.push(itemToAdd);
+    }
+    return myList;
+  }
+}
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function (myArray){
+    myArray.sort();
+    return myArray.reverse();
+}
