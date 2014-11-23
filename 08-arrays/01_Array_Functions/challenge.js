f785ece80cc4940de0a65793e6061f470c91ea0a
anomalophobe <01b6e6@gmail.com>
2014-11-23 05:08:00 +0000

    Updated Array Functions

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,25 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function (inArray){
+  var newArray = [];
+  var newVal = 0;
+  var arrLength = inArray.length;
+  for (var i = 0; i < arrLength; i++){
+    newVal = inArray.pop();
+    newArray.push(newVal);
+  }
+  newArray.unshift(1);
+  return newArray;
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function (inArray){
+  var arrString = inArray[0].toString();
+  var arrLength = inArray.length;
+  for (var i = 1; i < arrLength; i++){
+    arrString = arrString + "+" + inArray[i];
+  }
+  return arrString;
+}
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function (inArray){
+  var numItems = inArray.length + 1;
+  return numItems;
+}
