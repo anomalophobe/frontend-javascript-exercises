fc1f319768e5c6829bf23081cc470c5dd3a1d2a0
anomalophobe <01b6e6@gmail.com>
2014-11-24 20:50:46 +0000

    Updated Looping over arrays exercise

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,28 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function (numArray){
+  var x = numArray.length;
+  var sum = 0;
+  if (x === 0){
+    return sum;
+  } else {
+    for (var i = 0; i < x; i++){
+      sum += numArray[i];
+      }
+    return sum;
+  }
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function (inputString){
+  var newString = inputString.toLowerCase().split(" ");
+  return newString;
+};  
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function (inArray){
+  var x = inArray.length;
+  var outArray = [];
+  var tmpString = "";
+  for (var i = 0; i < x; i++){
+    tmpString = i + " is " + inArray[i].toString();
+    outArray.push(tmpString);    
+  }
+  return outArray;
+}
