112dc55c3154b89c602a9c20e5d6c669bc3f2742
anomalophobe <01b6e6@gmail.com>
2014-11-24 22:10:20 +0000

    Updated Iterating Over An Object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,28 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function (inObj) {
+  var newArr = [];
+  for (var p in inObj) {
+    newArr.push(p);
+  }
+  return newArr;
+}
 
-module.exports.getValues = undefined;
+module.exports.getValues = function (inObj) {
+  var newArr = [];
+  for (var p in inObj) {
+    newArr.push(inObj[p]);
+  }
+  return newArr;
+}
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function (inObj) {
+  var newArr = [];
+  if (inObj.length === 0) {
+    return newArr;
+  } else {
+    for (var p in inObj) {
+      newArr.push(p + " is " + inObj[p]);
+    }
+    return newArr;
+  }
+}
+
