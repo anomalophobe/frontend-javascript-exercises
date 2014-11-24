abe15553673ef505e5644fa29493b96d9674cb17
anomalophobe <01b6e6@gmail.com>
2014-11-24 18:19:48 +0000

    Updated Objects exercise

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function createCourse (courseTitle, courseDuration, courseStudents){
+  var courseObj = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return courseObj;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function (object, newProp, newValue){
+  var newObj = object;
+  for (var p in object){
+    if (object.p === newProp){
+      return object;
+    } else {
+      newObj[newProp] = newValue;
+      return newObj;
+    }
+  }
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function (letter){
+  var newLetter = "Hello " + letter.recipient + ",\n\n" + letter.msg + 
+      "\n\nSincerely,\n" + letter.sender;
+  return newLetter;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function (item, money){
+  var matrix = {
+    "MacBook Air" : 999,
+    "MacBook Pro" : 1299,
+    "Mac Pro" : 2499,
+    "Apple Sticker" : 1
+  };
+  return (matrix[item] <= money);
+}
