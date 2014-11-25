e3a6550f2a3c5152211d44dfe38c5ad40fa8cb8e
anomalophobe <01b6e6@gmail.com>
2014-11-25 03:02:42 +0000

    Updated Object Operations exercise

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,35 @@
-module.exports.copy = undefined;
+module.exports.copy = function (inObj) {
+  var newObj = {};
+  for (var p in inObj){
+    newObj[p] = inObj[p]
+  }
+  return newObj;
+}
 
-module.exports.extend = undefined;
+module.exports.extend = function (dest, src) {
+  var newObj = dest;
+  for (var p in src) {
+    newObj[p] = src[p];
+  }
+  return newObj;
+}
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function (inObj, inArr) {
+  var x = inArr.length;
+  var objCnt = 0;
+  var hitCnt = 0;
+  var newArr = [];
+  if (x === 0) {
+    return true;
+  } else {
+    for (var p in inObj) {
+      objCnt++
+      for (var i = 0; i < x; i++) {
+        if (inArr[i] === p) {
+          hitCnt++
+        }
+      }
+    }
+    return (hitCnt <= objCnt) && (hitCnt === x);
+  }
+}
