3b94ab31cb2c6d418c1fe28623d4a3963c926c8c
anomalophobe <01b6e6@gmail.com>
2014-11-25 03:34:39 +0000

    Completed While Loops exercise

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,18 @@
-module.exports.stream = undefined;
+module.exports.stream = function (conditionalFn, actionFn) {
+  while (conditionalFn()) {
+    actionFn();
+  }
+}
 
-module.exports.sumNumbers = undefined;
+
+module.exports.sumNumbers = function (numArray) {
+  var x = numArray.length;
+  var sum = 0;
+  var i = 0;
+  while (i < x) {
+    sum += numArray[i];
+    i++;
+  }
+  return sum;
+}
 
